8f809e83e7bfb0a26a488d3b5687ca66a451044d
Yuriy Shevela <g69dwine@gmail.com>
2023-07-23 13:18:55 +0300

    Added Seminar2 task

---
+++ Seminar2/Programm.java
@@ -0,0 +1,9 @@
+package Homeworks.Seminar2;
+
+public class Programm {
+    public static void main(String[] args) {
+        int[] arr = {12, 11, 13, 5, 4, 7};
+        PyramidSort.PyramidSort(arr); 
+        PyramidSort.printArray(arr);
+    }
+}
